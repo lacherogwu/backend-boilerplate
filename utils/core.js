ec8e36f8a2b60bfa1d9f1aa96d56fd976951f016
lachero-work <asaf@gwumkt.com>
2021-04-29 10:07:48 +0300

    added import default function

--- utils/core.js
+++ utils/core.js
@@ -2,24 +2,28 @@ import fs from 'fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+const dImport = async path => {
+	const { default: d } = await import(path);
+	return d;
+};
+
 const mapDirFiles = async (path, type, filter) => {
 	path = dirname(fileURLToPath(path));
 	const files = fs.readdirSync(path).filter(i => (filter ? i.includes(`${filter}_`) : i !== 'index.js'));
 
-	const promises = files.map(i => import(`${path}/${i}`));
+	const promises = files.map(i => dImport(`${path}/${i}`));
 	const result = await Promise.all(promises);
-	const mapped = result.map(i => i.default);
 
-	if (type === 'values') return mapped;
+	if (type === 'values') return result;
 
 	return files.map((name, i) => ({
-		[name.replace('.js', '')]: mapped[i],
+		[name.replace('.js', '')]: result[i],
 	}));
 };
 
 const getController = async path => {
 	const fileName = /[^/]*$/.exec(path)[0];
-	return (await import(`../controllers/${fileName}`)).default;
+	return await dImport(`../controllers/${fileName}`);
 };
 
 export { mapDirFiles, getController };
